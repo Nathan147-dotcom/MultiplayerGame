23b562d9917f154be776b146f545516b81562d56
Nathan147-dotcom <nathanmorales12mntp@gmail.com>
2026-03-03 18:47:55 -0800

    win conditions

---
+++ Assets/TicTacToe.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TicTacToe : MonoBehaviour
+{
+    Boolean checker;
+    int plusOne;
+
+    public Text btnText1 = null;
+    public Text btnText2 = null;
+    public Text btnText3 = null;
+    public Text btnText4 = null;
+    public Text btnText5 = null;
+    public Text btnText6 = null;
+    public Text btnText7 = null;
+    public Text btnText8 = null;
+    public Text btnText9 = null;
+
+    public Text btnResetGame = null;
+    public Text btnNewGame = null;
+    public Text msgFeedback = null;
+
+    public Text txtPlayerX;
+    public Text txtPlayerO;
+
+    public void Score(){
+        if(btnText1.text == "X" && btnText2.text == "X" && btnText3.text == "X"){
+            btnText1.color = Color.red;
+            btnText2.color = Color.red;
+            btnText3.color = Color.red;
+            msgFeedback.text = "Player X Wins";
+            plusOne = int.Parse(txtPlayerX.text);
+            txtPlayerX.text = Convert.ToString(plusOne + 1)
+        }
+
+        if(btnText1.text == "X" && btnText4.text == "X" && btnText7.text == "X"){
+            btnText1.color = Color.blue;
+            btnText4.color = Color.blue;
+            btnText7.color = Color.blue;
+            msgFeedback.text = "Player X Wins";
+            plusOne = int.Parse(txtPlayerX.text);
+            txtPlayerX.text = Convert.ToString(plusOne + 1)
+        }
+
+         if(btnText1.text == "X" && btnText5.text == "X" && btnText9.text == "X"){
+            btnText1.color = Color.green;
+            btnText5.color = Color.green;
+            btnText9.color = Color.green;
+            msgFeedback.text = "Player X Wins";
+            plusOne = int.Parse(txtPlayerX.text);
+            txtPlayerX.text = Convert.ToString(plusOne + 1)
+        }
+
+        if(btnText3.text == "X" && btnText5.text == "X" && btnText7.text == "X"){
+            btnText3.color = Color.gray;
+            btnText5.color = Color.gray;
+            btnText7.color = Color.gray;
+            msgFeedback.text = "Player X Wins";
+            plusOne = int.Parse(txtPlayerX.text);
+            txtPlayerX.text = Convert.ToString(plusOne + 1)
+        }
+
+        if(btnText2.text == "X" && btnText5.text == "X" && btnText8.text == "X"){
+            btnText2.color = Color.yellow;
+            btnText5.color = Color.yellow;
+            btnText8.color = Color.yellow;
+            msgFeedback.text = "Player X Wins";
+            plusOne = int.Parse(txtPlayerX.text);
+            txtPlayerX.text = Convert.ToString(plusOne + 1)
+        }
+
+        if(btnText3.text == "X" && btnText6.text == "X" && btnText9.text == "X"){
+            btnText1.color = Color.cyan;
+            btnText4.color = Color.cyan;
+            btnText7.color = Color.cyan;
+            msgFeedback.text = "Player X Wins";
+            plusOne = int.Parse(txtPlayerX.text);
+            txtPlayerX.text = Convert.ToString(plusOne + 1)
+        }
+
+         if(btnText4.text == "X" && btnText5.text == "X" && btnText6.text == "X"){
+            btnText4.color = Color.green;
+            btnText5.color = Color.green;
+            btnText6.color = Color.green;
+            msgFeedback.text = "Player X Wins";
+            plusOne = int.Parse(txtPlayerX.text);
+            txtPlayerX.text = Convert.ToString(plusOne + 1)
+        }
+
+        if(btnText7.text == "X" && btnText8.text == "X" && btnText9.text == "X"){
+            btnText7.color = Color.blue;
+            btnText8.color = Color.blue;
+            btnText9.color = Color.blue;
+            msgFeedback.text = "Player X Wins";
+            plusOne = int.Parse(txtPlayerX.text);
+            txtPlayerX.text = Convert.ToString(plusOne + 1)
+        }
+    }
+}
